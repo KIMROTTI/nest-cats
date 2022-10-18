70e3f782d22beafaba7f82375714b9e35011aaad
Rotti_Kim <andy_1009@naver.com>
2022-10-19 02:19:14 +0900

    cats first

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat-dto';
+import { Cat } from './interface/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+  //컨스트럭터 용도 data 의존성 주입? private => 선언 및 초기화
+  @Get()
+  findAll(): Cat[] {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() createCatDto: CreateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removees a #${id} cat`;
+  }
+}
---
+++ src/cats/middleware/logger.middleware.ts
@@ -0,0 +1,10 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Request...');
+    next(); //미들웨어 처리이후 꼭 next
+  }
+}
